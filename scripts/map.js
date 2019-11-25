b825f340a61b2c63b78b5c4d7e91f20129a9040f
EUinfo <gamesorli@gmail.com>
2019-11-25 11:41:39 +0100

    With Nodes 20191125

---
+++ scripts/map.js
@@ -0,0 +1,168 @@
+"use strict";
+console.log("connected");
+
+async function getUserAsync(urlString) 
+{
+  let response = await fetch(urlString);
+  let data = await response.json()
+  return data;
+}
+
+function geoJSONFilter(stops, locationTypeValue) {
+    let stop_icon = L.icon({
+        iconUrl: "icons/zastavka_kordis_100_100.svg",
+        iconSize: [15, 15],
+    });
+    let stopsFiltered = L.geoJSON(stops, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            let currentMarker = L.marker(latlng, { icon: stop_icon});
+            return currentMarker;
+        },
+        filter: function (feature) {
+            let coords= {}
+            coords.lng = feature.geometry.coordinates[0]
+            coords.lat = feature.geometry.coordinates[1]
+            if (feature.properties.location_type === locationTypeValue && MAP.getBounds().contains(coords)) {
+                return true;
+            } 
+        }
+    }).bindPopup(function (layer) {
+        let popup = layer.feature.properties.stop_name + "<br/>" + layer.feature.properties.stop_id
+        return popup;
+    }).on("click", function(e) {
+        console.log(e.layer.feature.properties.stop_id)
+    });
+    return stopsFiltered    
+}
+
+const MAP_ID = document.getElementById("m");
+const MAP = L.map(MAP_ID);
+
+const URL_MAPY_CZ = "https://mapserver.mapy.cz/base-m/{z}-{x}-{y}?s=0.2&dm=Luminosity";
+const MAPY_CZ = L.tileLayer(URL_MAPY_CZ, {
+    attribution: "<a href='https://mapy.cz/zakladni?'>Seznam.cz</a>"
+})
+
+MAP.addLayer(MAPY_CZ)
+MAP.setView([49.1946378, 16.6070083], 16)
+
+getUserAsync("https://raw.githubusercontent.com/zastavky/zastavky.github.io/master/geojson/stops.geojson")
+  .then(stops_json => {
+    let stopsDetail = geoJSONFilter(stops_json, 0).addTo(MAP)
+    for (stop in stopsDetail._layers) {
+        /*console.log(stopsDetail._layers[stop].feature.properties.stop_id)*/
+    }
+    
+    let stopsParents = geoJSONFilter(stops_json, 1)
+
+    let stopsCluster = L.markerClusterGroup({maxClusterRadius: 45, 
+        iconCreateFunction: function(cluster) {
+            /*let markersCount = cluster.getAllChildMarkers();
+            let html = "<b class='clusterText'>"+ markersCount.length + "</b>";*/
+            return L.divIcon({iconSize: [30,30], className: "clusterIcon"});
+        } });
+    MAP.addLayer(stopsCluster); 
+
+    MAP.on("zoom", function() {
+        console.log(MAP.getBounds())
+        if (MAP.getZoom() >= 16) { 
+            stopsDetail.remove();      
+            stopsDetail = geoJSONFilter(stops_json, 0).addTo(MAP);
+            stopsCluster.clearLayers();
+            
+        }
+        else if (MAP.getZoom() >= 10 && MAP.getZoom() < 16 ) {
+            stopsDetail.remove();
+            stopsParents = geoJSONFilter(stops_json, 1);
+            stopsCluster.clearLayers();            
+            stopsCluster.addLayer(stopsParents);                 
+           
+        }
+        else if (MAP.getZoom() < 10) {
+            stopsCluster.clearLayers();
+            stopsCluster.addLayer(stopsParents);
+        }
+    })
+    MAP.on("moveend", function() {
+        if (MAP.getZoom() >= 16) { 
+            stopsDetail.remove();      
+            stopsDetail = geoJSONFilter(stops_json, 0).addTo(MAP);
+            stopsCluster.clearLayers();
+            
+        }
+        else if (MAP.getZoom() >= 10 && MAP.getZoom() < 16 ) {
+            stopsDetail.remove();
+            stopsParents = geoJSONFilter(stops_json, 1);
+            stopsCluster.clearLayers();            
+            stopsCluster.addLayer(stopsParents);                 
+           
+        }
+        else if (MAP.getZoom() < 10) {
+            stopsCluster.clearLayers();
+            stopsCluster.addLayer(stopsParents);
+        }
+    })
+
+});
+
+
+/*
+fetch("https://raw.githubusercontent.com/zastavky/zastavky.github.io/master/geojson/stops.geojson")
+.then(response => response.json())
+.then(stops_json => {
+    let stopsDetail = geoJSONFilter(stops_json, 0).addTo(MAP)
+    for (stop in stopsDetail._layers) {
+        console.log(stopsDetail._layers[stop].feature.properties.stop_id)
+    }
+    
+    let stopsParents = geoJSONFilter(stops_json, 1)
+
+    let stopsCluster = L.markerClusterGroup({maxClusterRadius: 30, 
+        iconCreateFunction: function(cluster) {
+            let markersCount = cluster.getAllChildMarkers();
+            let html = "<b class='clusterText'>"+ markersCount.length + "</b>";
+            return L.divIcon({html: html, iconSize: [40,30], className: "clusterIcon"});
+        } });
+    MAP.addLayer(stopsCluster); 
+
+    MAP.on("zoom", function() {
+        if (MAP.getZoom() >= 16) { 
+            stopsDetail.remove();      
+            stopsDetail = geoJSONFilter(stops_json, 0).addTo(MAP);
+            stopsCluster.clearLayers();
+            
+        }
+        else if (MAP.getZoom() >= 10 && MAP.getZoom() < 16 ) {
+            stopsDetail.remove();
+            stopsParents = geoJSONFilter(stops_json, 1);
+            stopsCluster.clearLayers();            
+            stopsCluster.addLayer(stopsParents);                 
+           
+        }
+        else if (MAP.getZoom() < 10) {
+            stopsCluster.clearLayers();
+            stopsCluster.addLayer(stopsParents);
+        }
+    })
+    MAP.on("moveend", function() {
+        if (MAP.getZoom() >= 16) { 
+            stopsDetail.remove();      
+            stopsDetail = geoJSONFilter(stops_json, 0).addTo(MAP);
+            stopsCluster.clearLayers();
+            
+        }
+        else if (MAP.getZoom() >= 10 && MAP.getZoom() < 16 ) {
+            stopsDetail.remove();
+            stopsParents = geoJSONFilter(stops_json, 1);
+            stopsCluster.clearLayers();            
+            stopsCluster.addLayer(stopsParents);                 
+           
+        }
+        else if (MAP.getZoom() < 10) {
+            stopsCluster.clearLayers();
+            stopsCluster.addLayer(stopsParents);
+        }
+    })
+
+});*/
+
---
+++ scripts/script.js
@@ -0,0 +1,109 @@
+"use-strict";
+console.log("connected")
+
+function hideToolbar() {
+    document.getElementsByClassName("panel")[0].classList.toggle("hidden");
+    document.getElementById("button").classList.toggle("hidden_btn");
+    document.getElementsByClassName("main")[0].classList.toggle("hidden_panel");
+    MAP.invalidateSize({ animate: true });
+    console.log(document.getElementsByClassName("panel")[0].classList);
+}
+
+window.addEventListener("load", function (e) {
+    document.getElementById("intro").onclick = function () { switchIntro() };
+    document.getElementById("nodesBrno").onclick = function () { switchNodesBrno() };
+    document.getElementById("nodesRegion").onclick = function () { switchNodesRegion() };
+})
+
+function switchIntro() {
+    let menu_window_new = document.getElementById("menu-window")
+    let introText = "<p>Vítejte na stránkách pro vyhledávání zastávek v&nbsp;" +
+        "IDS JMK. Nyní se bude řešit optimalizace vykreslování zastávek (WebGL/shlukování) a&nbsp;" +
+        "stahování informací pro zobrazení odjezdů spojů</p>"
+    menu_window_new.innerHTML = introText
+}
+
+function switchNodesRegion() {
+    let menu_window_new = document.getElementById("menu-window")
+    let innerHTMLText = "<table class='nodesTable'>"
+    fetch("https://gist.githubusercontent.com/zastavky/f0f1165cc6af94483fcd9b4dda199539/raw/c33d45aa38115d54ae54f6834ae6a63e3568eed6/nodes.json")
+        .then(response => response.json())
+        .then(nodes => {
+            for (node in nodes) {
+                innerHTMLText += "<tr><td id=" + node + ">"
+                innerHTMLText += nodes[node].toUpperCase() + "</td></tr>"
+            }
+            innerHTMLText += "</table>"
+            menu_window_new.innerHTML = innerHTMLText
+
+            let newBounds
+            let newPoint
+            let lng
+            let lat
+            menu_window_new.addEventListener("click", function (e) {
+                fetch("https://raw.githubusercontent.com/zastavky/zastavky.github.io/master/geojson/stops.geojson")
+                    .then(response => response.json())
+                    .then(stops_json => {
+                        newBounds = []
+                        for (let i = 0; i < stops_json.features.length; i++) {
+                            if (stops_json.features[i].properties.stop_name.toUpperCase().includes(e.srcElement.innerHTML.toUpperCase() + ",")) {
+                                newPoint = []
+                                lat = stops_json.features[i].geometry.coordinates[1]
+                                newPoint.push(lat)
+                                lng = stops_json.features[i].geometry.coordinates[0]
+                                newPoint.push(lng)
+                                newBounds.push(newPoint)
+                            }
+                        }
+                        MAP.fitBounds(newBounds)
+                    }
+                    )
+
+            })
+        })
+}
+
+
+function switchNodesBrno() {
+    let menu_window_new = document.getElementById("menu-window")
+    let innerHTMLText = "<table class='nodesTable'>"
+    fetch("https://gist.githubusercontent.com/zastavky/2c0471269e67e21c0777f257f159ca59/raw/506dfb1041d3bd1f75de2b7a11d28ec7fb4a284d/nodesBrno.json")
+        .then(response => response.json())
+        .then(nodes => {
+            for (node in nodes) {
+                innerHTMLText += "<tr><td id=" + node + ">"
+                innerHTMLText += nodes[node].toUpperCase() + "</td></tr>"
+            }
+            innerHTMLText += "</table>"
+            menu_window_new.innerHTML = innerHTMLText
+
+            let newBounds
+            let newPoint
+            let lng
+            let lat
+            menu_window_new.addEventListener("click", function (e) {
+                fetch("https://raw.githubusercontent.com/zastavky/zastavky.github.io/master/geojson/stops.geojson")
+                    .then(response => response.json())
+                    .then(stops_json => {
+                        newBounds = []
+                        let stops
+                        stops = e.srcElement.innerHTML.toUpperCase().split(" + ")
+                        for (let i = 0; i < stops_json.features.length; i++) {
+                            for (let j = 0; j < stops.length; j++) {
+                                if (stops_json.features[i].properties.stop_name.toUpperCase() == stops[j]) {
+                                    newPoint = []
+                                    lat = stops_json.features[i].geometry.coordinates[1]
+                                    newPoint.push(lat)
+                                    lng = stops_json.features[i].geometry.coordinates[0]
+                                    newPoint.push(lng)
+                                    newBounds.push(newPoint)
+                                }
+                            }
+                        }
+                        MAP.fitBounds(newBounds)
+                    }
+                    )
+
+            })
+        })
+}
